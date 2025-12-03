e77a48f48dd4d35059653406b0342490e3b9bd6e
“Bogdan <bogdan.daniel.ioan@outlook.com>
2025-12-03 22:51:27 +0200

    fixed cors

--- src/main/java/mycode/online_shop_api/OnlineShopApiApplication.java
+++ src/main/java/mycode/online_shop_api/OnlineShopApiApplication.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
 
@@ -26,19 +26,48 @@ public class OnlineShopApiApplication {
         };
     }
     @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+    public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList(   "http://127.0.0.1:5500","http://localhost:5500","http://localhost:5173", "http://localhost" ,"http://localhost:3000", "http://localhost:8081" , "http://localhost:8080" , "http://localhost:8083"));
-        corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
-                "Accept", "Jwt-Token", "Authorization", "Origin, Accept", "X-Requested-With",
-                "Access-Control-Request-Method", "Access-Control-Request-Headers", "Access-Control-Allow-Headers"));
-        corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Jwt-Token", "Authorization",
-                "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials", "Access-Control-Allow-Headers"));
+
+        corsConfiguration.setAllowedOriginPatterns(Arrays.asList(
+                "http://127.0.0.1:*",
+                "http://localhost:*",
+                "https://127.0.0.1:*",
+                "https://localhost:*"
+        ));
+
+        corsConfiguration.setAllowedHeaders(Arrays.asList(
+                "Origin",
+                "Access-Control-Allow-Origin",
+                "Content-Type",
+                "Accept",
+                "Jwt-Token",
+                "Authorization",
+                "Origin, Accept",
+                "X-Requested-With",
+                "Access-Control-Request-Method",
+                "Access-Control-Request-Headers",
+                "Access-Control-Allow-Headers",
+                "Access-Control-Request-Private-Network"
+        ));
+
+        corsConfiguration.setExposedHeaders(Arrays.asList(
+                "Origin",
+                "Content-Type",
+                "Accept",
+                "Jwt-Token",
+                "Authorization",
+                "Access-Control-Allow-Origin",
+                "Access-Control-Allow-Credentials",
+                "Access-Control-Allow-Headers"
+        ));
+
         corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
         urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-        return new CorsFilter(urlBasedCorsConfigurationSource);
+        return urlBasedCorsConfigurationSource;
     }
     @Bean
     BCryptPasswordEncoder bCryptPasswordEncoder(){
